087eccad2ab4a9a9f671f999be6456496c99e549
Georgy-Smirnov <gogasmi@mail.ru>
2022-06-05 19:33:33 +0300

    up

--- select.c
+++ select.c
@@ -2,3 +2,29 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/time.h>
+
+int main(void) {
+	fd_set rd_fds;
+	struct timeval tv;
+
+	FD_ZERO(&rd_fds);
+	FD_SET(0, &rd_fds);
+	tv.tv_sec = 10;
+	tv.tv_usec = 0;
+	int max_num = 1;
+	int ret = select(max_num, &rd_fds, NULL, NULL, &tv);
+	if (ret) {
+		printf("Data is ready\n");
+		printf("FD_ISSET status: %u\n", FD_ISSET(0, &rd_fds));
+		if (FD_ISSET(0, &rd_fds)) {
+			char str[1024];
+			scanf("%s", str);
+			printf("%s\n", str);
+		}
+	}
+	else {
+		printf("TIMEOUT\n");
+	}
+	while(1);
+	return 0;
+}
